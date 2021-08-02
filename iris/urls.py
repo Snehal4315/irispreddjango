e156e8fe5c26f2c17b94d6cea21263b43eaf9046
Snehal4315 <gamitsnehal107@gmail.com>
2021-08-02 18:29:34 +0530

    commitenv11

--- iris/urls.py
+++ iris/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',views.index,name='index'),
     path('result',views.result,name='result'),
-] 
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
