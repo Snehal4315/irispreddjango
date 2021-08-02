9fee3b9c32750d319e0970239d80caa88f71dd02
Snehal4315 <gamitsnehal107@gmail.com>
2021-08-02 18:11:11 +0530

    commitenv10

--- mlprojects/settings.py
+++ mlprojects/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from decouple import config
+import mimetypes
+mimetypes.add_type("text/css", ".css", True)
 # import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
