f7a02bc1f3c4ba62bbaf10ea32a5a8c3b2a7992a
Snehal4315 <gamitsnehal107@gmail.com>
2021-08-06 11:37:12 +0530

    commitenv13

--- mlprojects/settings.py
+++ mlprojects/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['irispreddjango.herokuapp.com','localhost']
+ALLOWED_HOSTS = ['irispreddjango.herokuapp.com','127.0.0.1','localhost']
 
 
 # Application definition
